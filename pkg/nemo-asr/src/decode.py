0683384658c2e9c39721f2a014bc212cea12ea44
yuta0306 <yuta.20010306@gmail.com>
2024-08-17 12:27:53 +0900

    fix: long segments

--- pkg/nemo-asr/src/decode.py
+++ pkg/nemo-asr/src/decode.py
@@ -4,12 +4,11 @@ from .interface import Subword, Segment, TranscribeResult
 PAD_SECONDS = 0.5
 SECONDS_PER_STEP = 0.08
 SUBWORDS_PER_SEGMENTS = 10
-PHONEMIC_BREAK = 0.5
+PHONEMIC_BREAK = 1.5
 
 TOKEN_EOS = {'。', '?', '!'}
 TOKEN_COMMA = {'、', ','}
 TOKEN_PUNC = TOKEN_EOS | TOKEN_COMMA
-SKIP_TOKEN_IDS = {2}
 
 def find_end_of_segment(subwords, start):
     """Heuristics to identify speech boundaries"""
@@ -21,15 +20,10 @@ def find_end_of_segment(subwords, start):
             if nex.token not in TOKEN_PUNC:
                 if cur.token in TOKEN_EOS:
                     break
-                if cur.token in TOKEN_COMMA or nex.seconds - cur.seconds > PHONEMIC_BREAK:
-                    break
+                elif idx - start >= SUBWORDS_PER_SEGMENTS:
+                    if nex.seconds - cur.seconds > PHONEMIC_BREAK:
+                        break
     return idx
-
-def fix_punk_seconds(subwords):
-    for idx in range(len(subwords)):
-        subword = subwords[idx]
-        if subword.token in TOKEN_PUNC and idx > 0:
-            subwords[idx].seconds = subwords[idx - 1].seconds + SECONDS_PER_STEP
 
 def decode_hypothesis(model, hyp):
     """Decode ALSD beam search info into transcribe result
@@ -43,24 +37,20 @@ def decode_hypothesis(model, hyp):
     """
     # NeMo prepends a blank token to y_sequence with ALSD.
     # Trim that artifact token.
-    y_sequence: list = hyp.y_sequence.tolist()[1:]
+    y_sequence = hyp.y_sequence.tolist()[1:]
     text = model.tokenizer.ids_to_text(y_sequence)
 
     subwords = []
-    skip_indices = []
     for idx, (token_id, step) in enumerate(zip(y_sequence, hyp.timestep)):
-        if token_id in SKIP_TOKEN_IDS:  # skip "_" token
-            skip_indices.append(idx)
-            continue
         subwords.append(Subword(
             token_id=token_id,
             token=model.tokenizer.ids_to_text([token_id]),
             seconds=max(SECONDS_PER_STEP * (step - idx - 1) - PAD_SECONDS, 0)
         ))
-    for idx in reversed(skip_indices):  # remove "_" tokens
-        y_sequence.pop(idx)
 
-    fix_punk_seconds(subwords)
+    # In SentncePiece, whitespace is considered as a normal token and
+    # represented with a meta character (U+2581). Trim them.
+    subwords = [x for x in subwords if x.token]
 
     segments = []
     start = 0
@@ -69,7 +59,7 @@ def decode_hypothesis(model, hyp):
         segments.append(Segment(
             start_seconds=subwords[start].seconds,
             end_seconds=subwords[end].seconds + SECONDS_PER_STEP,
-            text=model.tokenizer.ids_to_text(y_sequence[start:end+1]),
+            text="".join(x.token for x in subwords[start:end+1]),
         ))
         start = end + 1
 
